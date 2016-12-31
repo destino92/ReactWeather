16fdb6f55f0d4186dc9551d9e597cbfe23425271
destino92 <destinodello@gmail.com>
2016-12-31 23:27:58 +0300

    chore: refactored stateless functional component to use ES6 arrow function

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,12 +1,9 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function(){
-    var {temp, location} = this.props;
+var WeatherMessage = ({temp, location}) => {
     return (
       <h1>It's {temp} in {location}</h1>
     )
-  }
-});
+};
 
 module.exports = WeatherMessage;
